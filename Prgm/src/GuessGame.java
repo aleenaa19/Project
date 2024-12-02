76909cd565846d0a34514d0b94fd7a25cafd7946
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-02 12:33:13 +0530

    GuessGame

---
+++ Prgm/src/GuessGame.java
@@ -0,0 +1,39 @@
+/*Pgm using looping statement
+===================
+Create a guessing game in which user can enter a number between 1 to 100.
+If the user given number is equal to program generated random number then he/she
+scores point otherwise try the next attempt.
+Each user can make five attempts.On correct guess will get 5 score otherwise 0.
+Output will be the score of user.(use for loop)
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessGame {
+    public static void main(String[] args) {
+
+
+        int i;
+        int points=0;
+        Scanner sc = new Scanner(System.in);
+
+        Random random = new Random();
+
+
+        for (i = 0; i < 5; i++) {
+            int ran = random.nextInt(100);
+            System.out.print("\nRandom Number:" +ran);
+            System.out.print("\nEnter a number:");
+            int n = sc.nextInt();
+            if (n == ran) {
+                points += 5;
+            } else {
+                System.out.print("Try next attempt\n");
+
+            }
+        }
+        System.out.print("Total points:" +points);
+
+    }
+}
