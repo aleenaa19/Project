07d9fc68615d3e02c69a6df37490a21b21e944e6
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-04 14:53:01 +0530

    validity

--- New/src/Validity.java
+++ New/src/Validity.java
@@ -1,5 +1,41 @@
+import java.util.*;
+
 public class Validity {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string with brackets:");
+        String s = sc.nextLine();
+        //char[] arr = s.toCharArray();
+        if(isValid(s)){
+            System.out.println("Valid");}
+        else{
+            System.out.println("Invalid");
+        }
     }
+        public static boolean isValid(String s) {
+            Stack<Character> stack = new Stack();
+
+            for (char c : s.toCharArray()) {
+                if (c == '(' || c == '{' || c == '[') {
+                    stack.push(c);
+                }
+                else if (c == ')' || c == '}' || c == ']') {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+
+                    char top = stack.pop();
+
+                    if (c == ')' && top != '(' ||
+                            c == '}' && top != '{' ||
+                            c == ']' && top != '[') {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.isEmpty();
+        }
+
+
 }
