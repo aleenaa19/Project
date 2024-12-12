2082f063412ae22bed792276d6accdbac87e6265
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-12 17:44:08 +0530

    Duplicate_ArrayInteger

--- New/src/Duplicate_Integers.java
+++ New/src/Duplicate_Integers.java
@@ -1,41 +1,45 @@
+
+
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Arrays;
 
+@FunctionalInterface
+interface Duplicates {
+    HashSet<Integer> store(int[] arr);
+}
 
-    @FunctionalInterface
-    interface Duplicates {
-        void store(Integer num);
-    }
 public class Duplicate_Integers {
 
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter the number of elements: ");
-            int size = scanner.nextInt();
+        System.out.print("Enter the number of elements: ");
+        int size = scanner.nextInt();
 
-            int[] arr = new int[size];
+        int[] arr = new int[size];
 
-            System.out.println("Enter the elements:");
+        System.out.println("Enter the elements:");
 
-            for (int i = 0; i < size; i++) {
-                arr[i] = scanner.nextInt();
-            }
-            HashSet<Integer> uniqueNumbers = new HashSet<>();
-
-                Duplicates adder = num -> uniqueNumbers.add(num);
-
-            for ( int i = 0; i < size; i++) {
-                adder.store(arr[i]);
-            }
-
-            Integer[] result = uniqueNumbers.toArray(new Integer[0]);
-            System.out.println("Array with unique elements: " + Arrays.toString(result));
-
-            scanner.close();
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
         }
-    }
 
+        Duplicates adder = (arrInput) -> {
+            HashSet<Integer> uniqueNumbers = new HashSet<>();
+            for (int num : arrInput) {
+                uniqueNumbers.add(num);
+            }
+            return uniqueNumbers;
+        };
+
+        HashSet<Integer> uniqueNumbers = adder.store(arr);
+
+        Integer[] result = uniqueNumbers.toArray(new Integer[0]);
+        System.out.println("Array with unique elements: " + Arrays.toString(result));
+
+        scanner.close();
+    }
+}
 
 
