f55286ad525b7cdb1668c73ecf97a2503959c824
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-13 14:57:42 +0530

    exception-handling

--- New/src/ExceptionHandling.java
+++ New/src/ExceptionHandling.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
-
+class DuplicateEmployeeFoundException extends Exception {
+    public DuplicateEmployeeFoundException(String message) {
+        super(message);  }
+}
 public class ExceptionHandling {
     public static void main(String[] args) {
 
@@ -10,7 +13,7 @@ public class ExceptionHandling {
         String[] employee = new String[100];
         double[] salary = new double[100];
         int i;
-        for ( i = 0; i < 3; i++) {
+        for ( i = 0; i < 5; i++) {
             try {
                 System.out.print("Enter name of employee" +(i+1)+ ":");
                 String name = sc.nextLine();
@@ -19,6 +22,12 @@ public class ExceptionHandling {
                 if (name==null || name.isEmpty()) {
                     throw new IllegalArgumentException("Name cannot be null or empty");
                 }
+                for (int j = 0; j < i; j++) {
+                    if (employee[j] != null && employee[j].equalsIgnoreCase(name)) {
+                        throw new DuplicateEmployeeFoundException("Employee name '" + name + "' already exists.");
+                    }
+                }
+
                 System.out.print("Enter salary of employee" +(i+1)+ ":");
                 String emp_sal = sc.nextLine();
                 double sal=Double.parseDouble(emp_sal);
@@ -39,10 +48,13 @@ public class ExceptionHandling {
             {
                 System.out.println("Error:" + e);
 
+            } catch (DuplicateEmployeeFoundException e) {
+                System.out.println("Error:" + e);
+
             }
         }
         System.out.println("\nSalary details");
-        for(i=0;i<3;i++)
+        for(i=0;i<5;i++)
         {
             System.out.println(employee[i] +" got salary " +salary[i]);
 
