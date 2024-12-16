1ed5b612793dd9512ad18f1d931051f33f5bb2f7
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-16 11:34:05 +0530

    linkedlist

--- New/src/Collections/Linklist.java
+++ New/src/Collections/Linklist.java
@@ -1,4 +1,12 @@
+/*
+1.Using LinkedList process all the incoming calls in a call center in first come first serve manner.You can use string as collection object.
+Add all incoming calls to the list .
+Remove the first call from list for processing
+If the list is empty print the message "all calls processed"
+Otherwise display all calls to be processed from the list.*/
+
 package Collections;
+
 
 import java.util.LinkedList;
 
