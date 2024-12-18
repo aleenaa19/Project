a76c16e61ad7897cd9c69f3f079e663a1393734b
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-18 16:42:32 +0530

    read_file

--- New/src/StreamAPI/ReadFile.java
+++ New/src/StreamAPI/ReadFile.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 public class ReadFile {
     public static void main(String[] args) {
         String filePath = "D:\\git_pract\\Project\\New\\src\\StreamAPI\\file_read.txt";
-
+        System.out.println("Contents in file:");
         try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
             lines.forEach(System.out::println);
         } catch (IOException e) {
