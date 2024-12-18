6bfa0542cb20e212259a5efa68bae6e239df786d
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-18 16:35:56 +0530

    read_file

---
+++ New/src/StreamAPI/ReadFile.java
@@ -0,0 +1,19 @@
+package StreamAPI;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        String filePath = "D:\\git_pract\\Project\\New\\src\\StreamAPI\\file_read.txt";
+
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
