3572b3ec0385e5ec182f1512b870ac9a2fe3e1a6
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-18 11:57:01 +0530

    multiply-stream

--- New/src/Multiply.java
+++ New/src/Multiply.java
@@ -7,6 +7,7 @@ public class Multiply {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
         int[] result = Arrays.stream(array).map(x -> x * 3).toArray();
+        System.out.println(Arrays.toString(result));
 
     }
 
