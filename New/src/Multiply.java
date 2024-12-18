8b2fddda820db4e6733f8e0a5a9437512766f1f9
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-18 11:56:26 +0530

    multiply-stream

---
+++ New/src/Multiply.java
@@ -0,0 +1,13 @@
+/*1.using stream map function multiply all elements in an array by 3?
+ */
+
+import java.util.Arrays;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] result = Arrays.stream(array).map(x -> x * 3).toArray();
+
+    }
+
+}
---
+++ New/src/Uppercase.java
@@ -0,0 +1,11 @@
+/*using stream map convert all elements in a string array to upper case?*/
+
+import java.util.Arrays;
+
+public class Uppercase {
+    public static void main(String[] args) {
+        String[] array={"apple,banana,orange"};
+        String[] result=Arrays.stream(array).map(x->x.toUpperCase()).toArray();
+    }
+
+    }
