d78aacd6f8cf3b24815ea237d8c81e986cb78551
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-18 12:20:55 +0530

    arraylength-stream

---
+++ New/src/ArrayLength.java
@@ -0,0 +1,14 @@
+/* 3.Using stream map find the length of all elements in the string array?
+ */
+
+import java.util.Arrays;
+public class ArrayLength {
+    public static void main(String[] args) {
+        String[] array = {"apple","banana","orange"};
+        System.out.println("Length of each element in array:");
+        Arrays.stream(array).map( x->x.length())
+                .forEach(System.out::println);
+
+    }
+
+    }
