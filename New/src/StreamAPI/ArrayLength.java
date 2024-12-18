f58163da341040877cff8854868f64c2281aaa16
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-18 12:28:54 +0530

    arraylength-stream

--- New/src/StreamAPI/ArrayLength.java
+++ New/src/StreamAPI/ArrayLength.java
@@ -1,14 +1,16 @@
-package StreamAPI;/* 3.Using stream map find the length of all elements in the string array?
+/* 3.Using stream map find the length of all elements in the string array?
  */
+package StreamAPI;
 
 import java.util.Arrays;
+
 public class ArrayLength {
     public static void main(String[] args) {
-        String[] array = {"apple","banana","orange"};
+        String[] array = {"apple", "banana", "orange"};
         System.out.println("Length of each element in array:");
-        Arrays.stream(array).map( x->x.length())
+        Arrays.stream(array).map(x -> x.length())
                 .forEach(System.out::println);
 
     }
 
-    }
+}
