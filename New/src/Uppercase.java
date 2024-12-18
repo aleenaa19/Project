cb409d2f2a229e524e149a2178aaaf31d122b153
aleenaa19 <aleenaaaa2005@gmail.com>
2024-12-18 12:01:22 +0530

    uppercase-stream

--- New/src/Uppercase.java
+++ New/src/Uppercase.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class Uppercase {
     public static void main(String[] args) {
         String[] array={"apple,banana,orange"};
-        String[] result=Arrays.stream(array).map(x->x.toUpperCase()).toArray();
+        String[] result=Arrays.stream(array)
+                .map(x->x.toUpperCase()).toArray(String[]::new);
+        System.out.println("Result in uppercase:" +Arrays.toString(result));
+
     }
 
     }
